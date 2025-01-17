6448655f53e3609079e752398c775cfbf4da94aa
in1dm3-a <in1dm3-a@HZ352035.elorrieta.int>
2025-01-17 13:02:02 +0100

    Segundo coment

--- Entornos/src/Prueba/Prueba.java
+++ Entornos/src/Prueba/Prueba.java
@@ -35,7 +35,6 @@ public class Prueba {
 			}
 			
 			
-			
 			@Override
 			public String toString() {
 				return "Alumno [dni=" + dni + ", nombre=" + nombre + ", apellidos=" + apellidos + ", grupo=" + grupo + "]";
